cda3cb008a91e67007baa97d502f21e56e11aa71
Riya258 <89601021+Riya258@users.noreply.github.com>
2023-02-06 15:21:32 +0530

    Add files via upload

---
+++ JSDay8.js
@@ -0,0 +1,140 @@
+//object.keys()
+
+//Object.keys() on an object
+// const person=
+// {
+//     name:'Riya',
+//     age:21,
+//     city:'Poanta Sahib'
+// };
+// const result=Object.keys(person);
+// console.log(result);
+
+//Object.keys() on an array
+// const numbers=[1,2,3,4,5];
+// console.log(Object.keys(numbers));
+
+//Object.keys() on a string
+// const name='Riya';
+// console.log(Object.keys(name));
+
+// const obj=
+// {
+//     10: 'A',
+//     5: 'B',
+//     15: 'C',
+//     1: 'D'
+// }
+// console.log(Object.keys(obj));
+
+
+
+
+//Object.values()
+
+//random key ordering
+// const obj=
+// {
+//     10: 'A',
+//     5: 'B',
+//     15: 'C',
+//     1: 'D'
+// }
+// console.log(Object.values(obj));
+//output will be arranged in key's numerical order
+
+// const obj1=
+// {
+//     name:'Riya',
+//     age:21
+// }
+// console.log(Object.values(obj1));
+
+
+
+
+//Object.entries()
+
+// const obj=
+// {
+//     1: 'A',
+//     2: 'B',
+//     3: 'C',
+//     4: 'D'
+// }
+// console.log(Object.entries(obj));
+// console.log(Object.entries(obj)[2]);
+
+
+
+
+//Hoisting
+//declare variable using var keyword.
+// show();
+// console.log(a);
+// var a=10;
+// function show()
+// {
+//     console.log("Hello!!");
+// }
+
+
+//declare variable using let keyword.
+// show();
+// console.log(a);
+// let a=10;
+// function show()
+// {
+//     console.log("Hello!!");
+// }
+
+
+
+
+//JS Variables: we use 4 ways to declare a variable: using let,var,const,nothing.
+
+//using var
+// var x=10;
+// var y=5;
+// var z=x+y;
+// console.log(z);
+
+//using let
+// let x=10;
+// let y=5;
+// let z=x+y;
+// console.log(z);
+
+//using const
+// const x=10;
+// const y=5;
+// const z=x+y;
+// console.log(z);
+
+//using nothing
+// x=10;
+// y=5;
+// z=x+y;
+// console.log(z);
+
+
+
+//JS let
+// let x=10;
+// function show()
+// {
+//     let x=2;
+//     console.log(x);
+// }
+// console.log(x);
+// show();
+
+
+
+//JS var
+// var x=2;
+// var x=1;
+// console.log(x);
+
+
+
